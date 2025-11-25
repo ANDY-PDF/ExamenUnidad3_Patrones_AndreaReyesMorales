91114d6778aea42ea9326e2d13f1b591beef476a
Andrea <l21212358@tectijuana.edu.mx>
2025-11-24 21:44:27 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppLectura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = " Aplicación de Lectura con Marcadores Inteligentes";
+
+            Reading app = new Reading();
+            app.Run();
+        }
+    }
+}
---
+++ ResaltadorTextocs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLectura
+{
+    public class ResaltadorTextocs : DecoradorSegmentoTexto
+    {
+        private ConsoleColor _resaltador;
+
+        public ResaltadorTextocs(ISegmentoTexto segment, ConsoleColor resaltador) : base(segment)
+        {
+            _resaltador = resaltador;
+            if (_segment is SegmentoTextoBasico basic)
+            {
+                basic.HighlightColor = _resaltador;
+            }
+        }
+
+        public override ConsoleColor? HighlightColor => _resaltador;
+    }
+}
+    
+

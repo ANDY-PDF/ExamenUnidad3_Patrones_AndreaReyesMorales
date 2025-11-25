9f5cf475de6de7e5139d448fc44c78f6ce0d9cf0
Andrea <l21212358@tectijuana.edu.mx>
2025-11-24 21:49:46 -0800

    Add files via upload

---
+++ src/FabricaTextoFormato.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLectura
+{
+    public class FabricaTextoFormato
+    {
+        private static FabricaTextoFormato _instance;
+        private Dictionary<int, TextoFormato> _formats = new Dictionary<int, TextoFormato>();
+
+        private FabricaTextoFormato() { }
+
+        public static FabricaTextoFormato Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new FabricaTextoFormato();
+                return _instance;
+            }
+        }
+
+        public TextoFormato GetFormat(string fontFamily, int fontSize, ConsoleColor textColor, bool isBold)
+        {
+            var format = new TextoFormato(fontFamily, fontSize, textColor, isBold);
+            int hash = format.GetHashCode();
+
+            if (!_formats.ContainsKey(hash))
+            {
+                _formats[hash] = format;
+            }
+
+            return _formats[hash];
+        }
+    }
+}
